279e4befd0bf2fc78a3cf4c62588afb4de13e3c9
Bruno Sousa <obssousa@pm.me>
2023-09-30 13:19:09 -0300

    refactor: change to Ionic

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,47 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ExploreContainer name="Tab 1 page" />
+    </ion-content>
+    <ion-fab slot="fixed" vertical="bottom" horizontal="end">
+      <ion-fab-button size="small">
+        <ion-icon :icon="add"></ion-icon>
+      </ion-fab-button>
+      <ion-fab-list side="end">
+        <ion-fab-button>
+          <ion-icon :icon="document"></ion-icon>
+        </ion-fab-button>
+        <ion-fab-button>
+          <ion-icon :icon="colorPalette"></ion-icon>
+        </ion-fab-button>
+        <ion-fab-button>
+          <ion-icon :icon="globe"></ion-icon>
+        </ion-fab-button>
+      </ion-fab-list>
+    </ion-fab>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
+import { IonFab, IonFabButton, IonFabList, IonIcon } from "@ionic/vue";
+import { add, colorPalette, document, globe } from "ionicons/icons";
+</script>
