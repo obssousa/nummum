a180624a9bd20117075e7947755447a2e237ee02
Bruno Sousa <obssousa@pm.me>
2023-09-29 21:50:23 -0300

    feature: add capacitor integration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,85 @@
 <script setup lang="ts">
-import { kApp, kPage, kNavbar, kBlock, kButton } from 'konsta/vue';
+ import { ref, watch } from 'vue';
+ import { Plus } from 'framework7-icons/vue';
+import { kApp, kPage, kNavbar, kFab, kBlock, kButton, kSearchbar, kList, kListItem } from 'konsta/vue';
+
+const items = [
+    { title: 'FC Ajax' },
+    { title: 'FC Arsenal' },
+    { title: 'FC Athletic' },
+    { title: 'FC Barcelona' },
+    { title: 'FC Bayern München' },
+    { title: 'FC Bordeaux' },
+    { title: 'FC Borussia Dortmund' },
+    { title: 'FC Chelsea' },
+    { title: 'FC Galatasaray' },
+    { title: 'FC Juventus' },
+    { title: 'FC Liverpool' },
+    { title: 'FC Manchester City' },
+    { title: 'FC Manchester United' },
+    { title: 'FC Paris Saint-Germain' },
+    { title: 'FC Real Madrid' },
+    { title: 'FC Tottenham Hotspur' },
+    { title: 'FC Valencia' },
+    { title: 'FC West Ham United' },
+  ];
+
+  const searchQuery = ref('');
+
+const handleSearch = (e: { target: { value: string; }; }) => {
+  searchQuery.value = e.target.value;
+};
+
+const handleClear = () => {
+  searchQuery.value = '';
+};
+
+const handleDisable = () => {
+  console.log('Disable');
+};
+
+const filteredItems = ref(items);
+
+watch(searchQuery, () => {
+  filteredItems.value = searchQuery.value
+    ? items.filter((item) =>
+        item.title.toLowerCase().includes(searchQuery.value.toLowerCase())
+      )
+    : items;
+});
 </script>
 
 <template>
-  <kApp theme="ios">
+  <kApp theme="material">
     <kPage>
       <kNavbar title="My App" />
+      <k-searchbar
+          :value="searchQuery"
+          disable-button
+          disable-button-text="Cancel"
+          @clear="handleClear"
+          @disable="handleDisable"
+          @input="handleSearch"
+        >
+        </k-searchbar>
+        <k-list strong inset-material outline-ios>
+      <k-list-item v-if="filteredItems.length === 0" title="Nothing found" />
+      <k-list-item
+        v-for="item in filteredItems"
+        :key="item.title"
+        :title="item.title"
+      />
+    </k-list>
       <kBlock>
-        <k-button>Button</k-button>
+        <k-button roundedMaterial>Button</k-button>
         <p>Here comes my app</p>
       </kBlock>
+
+      <k-fab class="fixed right-4-safe bottom-4-safe z-20">
+      <template #icon>
+        <component :is="Plus" />
+      </template>
+    </k-fab>
     </kPage>
   </kApp>
 </template>
---
+++ src/framework7-icons.d.ts
@@ -0,0 +1,4 @@
+declare module "framework7-icons/vue" {
+    const Plus: unknown;
+    const RocketFill: unknown;
+}
